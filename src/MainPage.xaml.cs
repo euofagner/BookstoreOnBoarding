79cc89627432d5a4126f14e05efb4094cfcc5366
Fagner <fagnergeo0@gmail.com>
2025-02-21 18:29:07 -0300

    changing the color of the last screen

--- src/MainPage.xaml.cs
+++ src/MainPage.xaml.cs
@@ -6,6 +6,8 @@ namespace src
 {
     public partial class MainPage : ContentPage
     {
+        private string? lblTitleColor = string.Empty;
+
         public MainPage(BookViewModel vm)
         {
             InitializeComponent();
@@ -25,6 +27,24 @@ namespace src
                     .SkipWhile(item => item != CarouselView.CurrentItem)
                     .Skip(1)
                     .First();
+            }
+        }
+
+        private void CarouselView_PositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            if (e.CurrentPosition == (int)CarouselView.ItemsSource.Cast<object>().Count() - 1 )
+            {
+                OnBoardingBtn.BackgroundColor = Color.FromRgb(24, 106, 185);
+                BackgroundColor = Color.FromRgb(177, 201, 236);
+                lblTitleColor = "#ffffff";
+                OnBoardingBtn.Text = "Cadastre - se";
+            }
+            else
+            {
+                OnBoardingBtn.BackgroundColor = Color.FromRgb(252, 134, 99);
+                BackgroundColor = Color.FromRgb(251, 225, 200);
+                lblTitleColor = "#fc8663";
+                OnBoardingBtn.Text = "Veja mais";
             }
         }
     }
