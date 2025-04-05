d90fee0c661cc61dc4187c6bdb4c957edf4d4f52
Fagner <fagnergeo0@gmail.com>
2025-04-05 12:00:26 -0300

    Adding 'keep connected checkbox' on the login screen

--- src/Views/HomePage.xaml.cs
+++ src/Views/HomePage.xaml.cs
@@ -1,13 +1,15 @@
-using System.Threading.Tasks;
-
 namespace src.Views;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public HomePage()
+    {
+        InitializeComponent();
+
+        Shell.SetBackgroundColor(this, Color.FromArgb("#eaeac3"));
+        Shell.SetTitleColor(this, Color.FromArgb("#333333"));
+        Shell.SetNavBarHasShadow(this, true);
+    }
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
