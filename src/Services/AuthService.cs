d16664e034235732afbd8f243697739331406d7f
Fagner <fagnergeo0@gmail.com>
2025-04-05 21:53:10 -0300

    add basic AuthService with mock authentication

--- src/Services/AuthService.cs
+++ src/Services/AuthService.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace src.Services
+namespace src.Services;
+
+public class AuthService
 {
-    class AuthService
+    private const string AuthStateKey = "AuthState";
+
+    public async Task<bool> IsAuthenticatedAsync()
     {
+        await Task.Delay(1000);
+
+        var authState = Preferences.Default.Get<bool>(AuthStateKey, false);
+        return authState;
+    }
+
+    public void Login()
+    {
+        Preferences.Default.Set<bool>(AuthStateKey, true);
+    }
+
+    public void Logout() 
+    {
+        Preferences.Default.Remove(AuthStateKey);
     }
 }
